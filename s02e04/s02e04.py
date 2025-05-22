7dc207e0572e82fcac3cca0af7e0f1afe94f1884
bartosz-bartosz <badalecki@gmail.com>
2025-05-22 18:44:01 +0200

    feat: s02e04 init

---
+++ s02e04/s02e04.py
@@ -0,0 +1,18 @@
+import json
+import logging
+import os
+
+from clients.centrala_client import CentralaClient
+from clients.local_llm_client import LocalLLMClient
+from clients.openai_client import ChatConfig, OpenAIClient
+
+
+FILES_PATH = "/Users/mcbartop/Code/ai_devs_3/downloaded_data/pliki_z_fabryki"
+
+
+class TaskSolver:
+    pass
+
+
+def main():
+    pass
